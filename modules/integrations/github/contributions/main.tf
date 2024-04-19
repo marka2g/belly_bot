d6d25f1a51fb797261d4664ce45734efd91b1549
Mark Sadegi <mark.sadegi@gmail.com>
2024-04-19 11:27:14 -0700

    grrrrr, completely remove branch protection for now

--- modules/integrations/github/contributions/main.tf
+++ modules/integrations/github/contributions/main.tf
@@ -10,17 +10,17 @@ provider "github" {
   owner = var.github_owner
 }
 
-resource "github_branch_protection" "main" {
-  repository_id = var.repository
-  pattern        = "main"
-  lock_branch    = true
-  enforce_admins = true
-  required_status_checks {
-    strict   = true
-    contexts = var.status_checks
-  }
-  # required_pull_request_reviews {
-  #   require_code_owner_reviews        = false
-  #   required_approving_review_count = 1
-  # }
-}
+# resource "github_branch_protection" "main" {
+#   repository_id = var.repository
+#   pattern        = "main"
+#   lock_branch    = true
+#   enforce_admins = true
+#   required_status_checks {
+#     strict   = true
+#     contexts = var.status_checks
+#   }
+#   required_pull_request_reviews {
+#     require_code_owner_reviews        = false
+#     required_approving_review_count = 1
+#   }
+# }
