cdab28e168c4ce0e2e1165e9533de7572e2e4074
Mark Sadegi <mark.sadegi@gmail.com>
2024-04-20 14:03:55 -0700

    Re Add GH Deployment Keys

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -7,6 +7,19 @@ module "swarm" {
   private_key_path = "${path.module}/dynamic_private_key.pem"
 }
 
+module "repository_secrets" {
+  source = "../../modules/integrations/github/secrets" 
+  secrets = {
+    "PRIVATE_KEY" = module.swarm.private_key, 
+    "AWS_ACCESS_KEY_ID" = var.aws_access_key_id, 
+    "AWS_SECRET_ACCESS_KEY" = var.aws_secret_access_key,
+    "AGE_KEY" = var.age_key,
+    "GH_PAT" = var.gh_pat
+  }
+  repository  = "belly_bot"
+  github_owner = "marka2g"
+}
+
 # can also run terraform output
 output "swarm_ssh_command" {
   value = module.swarm.ssh_command
